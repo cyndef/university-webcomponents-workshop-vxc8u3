54f83f682c7ab320c150390cf3f9da773a2cd08d
cyndef <cys7@psu.edu>
2022-09-30 11:00:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# university-webcomponents-workshop-vxc8u3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/university-webcomponents-workshop-vxc8u3)
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './u-alerts.js';
+import './u-mark.js';
+import './u-navbar.js';
+import './u-search.js';
+import './u-social-icon.js';
+import './u-toast.js';
+import './u-footer.js';
